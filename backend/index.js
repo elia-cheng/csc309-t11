dca7df91dfad37c5824d9c7d8e3f5e0eb2906cfc
Elia Cheng <elia.cheng@mail.utoronto.ca>
2025-11-19 02:10:08 -0500

    force add

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,19 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(cors({
   origin: FRONTEND_URL,
-  methods: 'GET,POST,DELETE',
+  methods: ['GET,POST,PUT,PATCH,DELETE,OPTIONS'],
   credentials: true
 }));
 
 app.options("*", cors());
+
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 app.use('', routes);
