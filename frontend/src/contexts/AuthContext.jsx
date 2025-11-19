7c92fcc92479a0dcbed89a18505cddd75a38cf3b
Elia Cheng <elia.cheng@mail.utoronto.ca>
2025-11-19 01:14:05 -0500

    trying something else

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext(null);
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // TODO: get the BACKEND_URL.
 
 /*
