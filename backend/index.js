85a3e7d63be2c453cc7098f06141ab72ea4c3145
Elia Cheng <elia.cheng@mail.utoronto.ca>
2025-11-19 02:00:52 -0500

    last attempt of the day

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import routes from "./routes.js";
-import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+const cors = require('cors');
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 // TODO: complete me (CORS)
 
 app.use(cors({
   origin: FRONTEND_URL,
+  methods: 'GET,POST,DELETE',
   credentials: true
 }));
 
@@ -23,7 +24,6 @@ app.use('', routes);
 app.get('/', function (req, res){
     res.send({msg: 'Running with CORS enabled'})
 })
-
 
 app.use((err, req, res, next) => {
   console.error(err);
